fe1f2ccecb104604349f09026e6fa3901eced016
galaterro <adrix1992@gmail.com>
2016-12-16 23:55:15 +0100

    Final commit, all exercises done.

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,10 @@ public class Main {
                 while(rs.next()){
                     System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" +rs.getString(4) + "\t" );
                 }
-                i = con.ejecutarInsert("delete from socios where Fecha_alta  < CDATE('2013-01-01 00:00:00') and Fecha_alta > CDATE('2011-31-12 00:00:00')");
+                i = con.ejecutarInsert("delete * from socios where Fecha_alta like '%2012%'");
+                System.out.println(i);
                 i = con.ejecutarInsert("Insert into socios(IdSocio,Nombre,Telefono,Fecha_alta) values ('9', 'Lucia', '912654215', #2017-01-01 00:00:00#)");
+                System.out.println(i);
             }else{
                 System.out.println("error");
             }
